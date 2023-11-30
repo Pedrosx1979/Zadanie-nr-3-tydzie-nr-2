ba86568c3b9aaf15462ea18e804537bc4ccc4614
Piotr Szydlak <pedrosx@o2.pl>
2023-11-30 10:26:58 +0100

    Add project files.

---
+++ Zadanie nr 3 tydzień nr 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_nr_3_tydzień_nr_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj długość boku Prostkąta");
+            double lenght;
+            bool option = double.TryParse(Console.ReadLine(), out lenght);
+            Console.WriteLine("Podaj szerokość boku Prostokąta");
+            double widht;
+            bool option2 = double.TryParse(Console.ReadLine(), out widht);
+            double diagonal = Math.Sqrt(Math.Pow(lenght, 2) + Math.Pow(widht, 2));
+            Console.WriteLine($"Długość przekątnej wynosi: {diagonal}");
+        }
+    }
+}
